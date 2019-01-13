b059d328b79db3d3993e9107d48b590e31a33198
Tran Lam <tranlamn81@gmail.com>
2019-01-13 01:21:52 -0800

    add 2 from local

--- file1.py
+++ file1.py
@@ -2,3 +2,11 @@ print("add 2 - from local")
 print("add 1 - from online")
 print("add 2 - from online")
 print("add 2 - from local")
+
+
+def myfunc1(mvar1):
+    print("add 3 from local")
+
+
+def myfunc2(mvar2):
+    print("add 3 from local")
