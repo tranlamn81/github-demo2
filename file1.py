66558c1b655ae2178545ff3dc3c4b0f4afae4aff
Tran Lam <tranlamn81@gmail.com>
2019-01-13 01:46:35 -0800

    revert

--- file1.py
+++ file1.py
@@ -4,17 +4,24 @@ print("add 2 - from online")
 print("add 2 - from local")
 
 
-def myfunc1(mvar1):
+def myfunc1(mvar1, mvaronline2):
+    print("add 4 from online")
     print("add 3 from local")
     print("add 4 from local")
+    print("add 4 from online")
 
 def myfunc2(mvar2):
-    print("add 3 from local")
+    print("add 4 from online")
+    print("add 3 from local-> edit 4 from online")
     print("add 4 from local")
 
 def myfunc3(mvar1):
     print("add 3 from online")
-
+    print("add 5 from local")
 
 def myfunc4(mvar2):
     print("add 3 from online")
+
+
+def myfunc6(mvar):
+    print("add 5 from local")
