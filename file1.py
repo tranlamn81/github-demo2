134c848ab67dd18e1f00f649147981c8ab1257dc
tranlamn81 <46367026+tranlamn81@users.noreply.github.com>
2019-01-13 01:21:32 -0800

    add 3 from online

--- file1.py
+++ file1.py
@@ -2,3 +2,10 @@ print("add 2 - from local")
 print("add 1 - from online")
 print("add 2 - from online")
 print("add 2 - from local")
+
+def myfunc3(mvar1):
+    print("add 3 from online")
+    
+def myfunc4(mvar2):
+    print("add 3 from online")
+    
