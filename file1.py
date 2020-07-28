d1be785db36a6ab5eec3b18c98db0713ba08c657
tranlamn81 <46367026+tranlamn81@users.noreply.github.com>
2020-07-28 01:02:43 -0700

    feature B new

--- file1.py
+++ file1.py
@@ -18,7 +18,7 @@ def myfunc2(mvar2):
 def myfunc3(mvar1):
     print("add 3 from online")
     print("add 5 from local")
-    # feature B
+    # feature B new
 
 def myfunc4(mvar2):
     print("add 3 from online")
